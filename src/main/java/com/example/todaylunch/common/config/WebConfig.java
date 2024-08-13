936bece7c7f1fcf72aff5644fff0e648000af1e0
EunHakKim <acky529@gmail.com>
2024-08-13 17:49:32 +0900

    fix: WebConfig 수정

--- src/main/java/com/example/todaylunch/common/config/WebConfig.java
+++ src/main/java/com/example/todaylunch/common/config/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(frontUri)
+                .allowCredentials(true)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
